b7834e252ea3aa7cc21850d54c04f07f6dd9c1e8
adrian3332 <74941032+adrian3332@users.noreply.github.com>
2021-03-19 22:37:59 -0500

    Delete N4 PROYECTO FINAL directory

--- N4 PROYECTO FINAL/claseDetVenta.py
+++
@@ -1,9 +0,0 @@
-from claseMarca import Marca
-from claseGrupo import Grupo
-from claseProducto import Producto
-class DetVenta:
-    def __init__(self, id, produc, prec=0, cant=0):
-        self.id=id
-        self.producto=produc
-        self.precio=produc.precio
-        self.cantidad=cant
--- N4 PROYECTO FINAL/claseMarca.py
+++
@@ -1,8 +0,0 @@
-class Marca:
-    def __init__(self, codigo, des, est):
-        self.id=codigo
-        self.descripcion=des
-        self.estado=est
-    
-    def mostrarMarca(self):
-        return " {};{};{} ".format(self.id, self.descripcion, self.estado)
