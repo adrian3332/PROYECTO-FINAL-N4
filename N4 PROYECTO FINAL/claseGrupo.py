0333e7bfe3ea09121ae30a4b92089fc61d761478
adrian3332 <74941032+adrian3332@users.noreply.github.com>
2021-03-19 22:49:38 -0500

    Delete N4 PROYECTO FINAL directory

--- N4 PROYECTO FINAL/claseGrupo.py
+++
@@ -1,8 +0,0 @@
-class Grupo:
-    def __init__(self, codigo, des, est= True):
-        self.id=codigo
-        self.descripcion=des
-        self.estado=est
-    
-    def mostrarGrupo(self):
-        return " {};{};{} ".format(self.id, self.descripcion, self.estado)
